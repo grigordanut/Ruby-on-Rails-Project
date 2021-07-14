d79d66808489ba2638b110c8050f0e58d6747561
grigordanut <danut.grigor@gmail.com>
2021-07-14 02:22:56 +0100

    First Commit

---
+++ db/migrate/20210702081927_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :product_name
+      t.text :description
+      t.string :size
+      t.decimal :price
+      t.text :comments
+
+      t.timestamps
+    end
+  end
+end
