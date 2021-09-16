08cbe75e3bf71b60e55a1395d2e1daa126d877bf
grigordanut <danut.grigor@gmail.com>
2021-09-16 07:02:42 +0100

    Update Users table

--- db/migrate/20210901205524_add_clothing_preference_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddClothingPreferenceToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :clothing_preference, :string
-    add_index :users, :clothing_preference
-  end
-end
--- db/migrate/20210916051318_remove_clothing_preference_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveClothingPreferenceFromUsers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :clothing_preference, :string
-  end
-end
